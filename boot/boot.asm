c52414e71cb493e7074ad46d732441a099430989
alvanrahimli <alvan@rahim.li>
2021-08-16 23:24:12 +0400

    Read 1 sector and print + error handling

--- boot/boot.asm
+++ boot/boot.asm
@@ -7,11 +7,6 @@ _start:
 times 33 db 0       ; Workaround for some BIOSes replacing data (BIOS Parameter Block) 
                     ; in sector (to make USB bootable, i guess)
 
-handle_zero:
-    mov si, ivt_zero_msg
-    call print_str
-    iret
-
 start:
     jmp 0x7c0:step2
 
@@ -25,22 +20,31 @@ step2:
     mov sp, 0x7c00
     sti             ; Enable interrupts
 
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7c0
+    mov ah, 2       ; Read sector command for int13
+    mov al, 1       ; Read 1 sector
+    mov ch, 0       ; Cylinder low 8 bits
+    mov cl, 2       ; Read sector 2
+    mov dh, 0       ; Head number
 
-    int 0
+    mov bx, buffer
+    int 0x13
 
-    mov si, message
-    call print_str
-    mov si, msg2
+    jc error
+
+    mov si, buffer
     call print_str
     jmp $           ; Forever loop
 
+error:
+    mov si, read_error_msg
+    call print_str
+    jmp $
+
 %include "print_str.asm"
 
-ivt_zero_msg: db 'This is IVT!', 0
-message: db 'Custom bootloader!', 0
-msg2: db 'Saalaam :D', 0
+read_error_msg: db 'Could not read data!', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+buffer: 
